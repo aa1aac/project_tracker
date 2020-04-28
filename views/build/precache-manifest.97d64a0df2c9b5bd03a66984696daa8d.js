4cb2b95bb14c9f91f3a0602c6d8cc266118c3fbe
aa1aac <ach.aayush.111@gmail.com>
2020-04-28 12:32:51 +0545

    minor fixes

---
+++ views/build/precache-manifest.97d64a0df2c9b5bd03a66984696daa8d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9fc9b5d543708ba019f895ea5188708a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "aeb7478a0b00b6c6e19b",
+    "url": "/static/css/2.352a0142.chunk.css"
+  },
+  {
+    "revision": "33d0ebb841f25368e583",
+    "url": "/static/css/main.8c7b6503.chunk.css"
+  },
+  {
+    "revision": "aeb7478a0b00b6c6e19b",
+    "url": "/static/js/2.5e84f5f1.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.5e84f5f1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "33d0ebb841f25368e583",
+    "url": "/static/js/main.4c958057.chunk.js"
+  },
+  {
+    "revision": "871b653b7f2255b8bd3d",
+    "url": "/static/js/runtime-main.b7f899a4.js"
+  },
+  {
+    "revision": "18b46b3e9fe20e5d255a708c7b31bec5",
+    "url": "/static/media/project.18b46b3e.jpg"
+  }
+]);
